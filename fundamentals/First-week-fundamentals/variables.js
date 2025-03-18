f9f43b9fe04140ddc4680ebb38eec745be17866f
Wambu-i7 <wambuindikiru@gmail.com>
2025-03-18 22:32:13 +0300

    Data types

---
+++ fundamentals/First-week-fundamentals/variables.js
@@ -0,0 +1,17 @@
+console.log('Hello\'s, world');
+const name = 'Rose';
+let age = 28;
+var color = 'peach';
+console.log(`My name is ${name}. I am ${age} years old. I love color ${color}.`);
+
+let n = 20;
+let word = 'beauty';
+let boo = true;
+let a = null;
+let b = undefined;
+
+console.log(typeof(n));
+console.log(typeof(word));
+console.log(typeof(boo));
+console.log(typeof(a));
+console.log(typeof(b));
