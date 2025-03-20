d1df0144b81932d0465ee0dc677554166230f0ff
Wambu-i7 <wambuindikiru@gmail.com>
2025-03-20 17:06:44 +0300

    function basics

---
+++ fundamentals/First-week-fundamentals/functions.js
@@ -0,0 +1,13 @@
+function getMin(a, b) {
+    if (a > b) {
+        return b;
+    } else {
+        return a;
+    }
+}
+console.log(getMin(2, 5));
+
+function pow(x, n) {
+    return x ** n;
+}
+console.log(pow(3, 2));
