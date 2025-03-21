7ec93c9b7dc2b24cf5288d91a5b0c90487cb112e
Wambu-i7 <wambuindikiru@gmail.com>
2025-03-21 17:07:49 +0300

    regular functions and arrow functions basics

--- fundamentals/First-week-fundamentals/functions.js
+++ fundamentals/First-week-fundamentals/functions.js
@@ -11,3 +11,19 @@ function pow(x, n) {
     return x ** n;
 }
 console.log(pow(3, 2));
+
+//Arrow functions
+//square
+const square = num => num * num;
+console.log(square(4));
+
+const greet = name => `Hello ${name}!`;
+console.log(greet('Rose'));
+
+setTimeout(() => {
+    console.log("Time's up!");
+  }, 2000);
+  
+const multiply = (a, b) => a * b;
+console.log(multiply(3, 5)); 
+
